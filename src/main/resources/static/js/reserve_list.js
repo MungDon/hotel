ec4bbab8f163d678376929976e0739fc889f5347
MungDon <sungminhyeong@naver.com>
2024-08-30 17:37:42 +0900

    예약 상세보기하는중

--- src/main/resources/static/js/reserve_list.js
+++ src/main/resources/static/js/reserve_list.js
@@ -1,4 +1,6 @@
 $(function(){
+    const modalInnerElement = $(".innerElement");   // 모달 내용 요소
+    const modal = $("#modalCon");                   // 모달 요소
     $(".reservation_type").on("change",(event)=>{
         const reservationType = $(event.target).val();
         if(isNull(reservationType)){
@@ -33,5 +35,36 @@ $(function(){
 
         // 갱신된 URL 반환
         return urlObj.toString();
+    }
+    $(".room_name").click((event)=>{
+        const reserveSid = $(event.target).data("reserve-sid");
+
+        const ajaxObj = {
+            url : API_LIST.RESERVE_DETAIL_USER,
+            method : "get",
+            param : {
+                reserveSid : reserveSid
+            },
+            successFn : (reserveDetail) => {
+                if(!isNull(reserveDetail)){
+                    const thenFn = () => {
+                        location.reload();
+                    }
+                    swalCall("에러","상세보기 데이터  가져오기 실패", "error",thenFn);
+                }
+                createReserveDetail(reserveDetail);
+            }
+        }
+        ajaxCall(ajaxObj);
+    });
+
+    const createReserveDetail = (reserveDetail) => {
+        modalInnerElement.empty();
+        const reserveDetailHTML =
+            `
+            
+            `;
+        modalInnerElement.append(reserveDetailHTML);
+        openModal(modal);
     }
 });
